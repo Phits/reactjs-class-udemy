902dd6b4f90ec1930ebf1eaa15fbe37bb43d1ff9
phits <john@fitz.com>
2015-10-07 16:13:48 -0400

    Finished section 3 - Tooling

--- thumbnail-gulp/src/app.jsx
+++ thumbnail-gulp/src/app.jsx
@@ -1,23 +1,21 @@
+var React = require('react');
+var ThumbnailList = require('./thumbnail-list');
+
 var options = {
   thumbnailData: [{
     title: "Show Courses",
-    number: 12,
+    number: 120,
     header: 'Learn React',
     description: 'React is fantastic library.',
     imageURL: 'http://formatjs.io/img/react.svg'
   },{
     title: "Show Courses",
-    number: 25,
+    number: 215,
     header: 'Learn Gulp',
     description: 'Gulp speeds up your development workflow.',
     imageURL: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400'
   }]
 };
 
-console.log('hey');
-
-// React, please render Class
 var element = React.createElement(ThumbnailList, options);
-
-// React, after you render, place in body tag
 React.render(element, document.querySelector('.target'));
--- thumbnail-gulp/src/badge.jsx
+++ thumbnail-gulp/src/badge.jsx
@@ -1,6 +1,7 @@
-var Badge = React.createClass({
-	render: function() {
+var React = require('react');
 
+module.exports = React.createClass({
+	render: function() {
 	  return <button className="btn btn-primary" type="button">
 	    {this.props.title} <span className="badge">{this.props.number}</span>
 	</button>
